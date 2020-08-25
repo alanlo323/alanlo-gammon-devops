e1efad7e014ba53420761d7f39f49b9b593975db
Alan Lo <alanlo@gammonconstruction.com>
2020-08-25 11:57:31 +0800

    Add project files.

---
+++ alanlo-gammon-devops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace alanlo_gammon_devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
