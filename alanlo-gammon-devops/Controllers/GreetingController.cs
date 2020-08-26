36230b823651472e9db29f5a4cb933833a7c25dc
Alan Lo <alanlo@gammonconstruction.com>
2020-08-26 11:57:56 +0800

    string input

--- alanlo-gammon-devops/Controllers/GreetingController.cs
+++ alanlo-gammon-devops/Controllers/GreetingController.cs
@@ -8,12 +8,12 @@ namespace alanlo_gammon_devops.Controllers
     [ApiController]
     public class GreetingController : ControllerBase
     {
-        public string Get(int id)
+        public string Get(string id)
         {
             TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
             configuration.InstrumentationKey = "61b7fb78-d207-411d-b7c5-59df48e5703f";
             var telemetryClient = new TelemetryClient(configuration);
-            telemetryClient.TrackTrace($@"4.0 Greeting Controller Get Id: {id}");
+            telemetryClient.TrackTrace($@"Alan Greeting Controller Get Id: {id}");
             return "Hello World 4.0!";
         }
     }
